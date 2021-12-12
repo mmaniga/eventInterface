cd5b5a9639a7a83c6eb1eadf2d8c18905062928c
Manigandan M <manigandanm@Manigandans-MacBook-Pro-3.local>
2021-12-12 15:13:55 +0530

    cors fix

--- src/main/java/com/c1exchange/meta/EventsInterface/config/CorsFilterConfiguration.java
+++ src/main/java/com/c1exchange/meta/EventsInterface/config/CorsFilterConfiguration.java
@@ -1,10 +1,11 @@
 package com.c1exchange.meta.EventsInterface.config;
-/*
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,16 +14,14 @@ import java.util.List;
 @Configuration
 @EnableWebMvc
 public class CorsFilterConfiguration  implements WebMvcConfigurer {
-    @Value( value = "${app.cors.allowed-origins}")
+    @Value(value = "${app.cors.allowed-origins}")
     private String allowedOrigins;
-
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         List<String> allowedOriginsArray = Arrays.asList(allowedOrigins.split(","));
-        System.out.println("allowedOriginsArray = " + allowedOriginsArray.toString());
-        //List<String> allowedOriginsArray = new ArrayList<>();
-        allowedOriginsArray.add("localhost");
+        System.out.println("allowedOriginsArray = " + allowedOrigins.toString());
+        //allowedOriginsArray.add("localhost2");
         registry.addMapping("/**")
                 .allowedHeaders("*")
                 .allowedOriginPatterns(allowedOriginsArray.toString())
@@ -30,4 +29,4 @@ public class CorsFilterConfiguration  implements WebMvcConfigurer {
     }
 
 }
-*/
+
